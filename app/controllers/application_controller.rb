83f69528bc102cdd17cb3dea4befc4157445ee3b
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-08-09 11:44:28 -0400

    Include error message if id not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,21 @@ class ApplicationController < Sinatra::Base
   # include show routes
   get "/aspects/:id" do
     aspect = Aspect.find_by(id: params[:id])
+    if aspect
     aspect.to_json(include: :strategies)
+    else
+      "404 - not found"
+    end
   end
 
   get "/strategies/:id" do
     strategy = Strategy.find_by(id: params[:id])
+    if strategy
     strategy.to_json(include: :aspect)
+    else 
+      "404 - not found"
+    end
   end
+
 
 end
