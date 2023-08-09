5e230436c4c29f1fecb973c9c7c55e3e47baf4c1
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-08-09 11:36:20 -0400

    Include show and index routes in application/controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < Sinatra::Base
     { message: "Let's create an amazing webapp!" }.to_json
   end
 
+  get "/aspects" do 
+    aspects = Aspect.all
+    aspects.to_json(include: :strategies)
+  end
+
+  get "/strategies" do 
+    strategies = Strategy.all
+    strategies.to_json(include: :aspect)
+  end
+
 end
