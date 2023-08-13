0a554687b6337e3077ffcd0f25663dcb262b1972
Tolulope Awoyomi <tolulopeawoyomi1@gmail.com>
2023-08-12 23:41:50 -0400

    Code cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,31 +10,6 @@ class ApplicationController < Sinatra::Base
     aspects.to_json(include: :strategies)
   end
 
-  # get "/strategies" do 
-  #   strategies = Strategy.all
-  #   strategies.to_json(include: :aspect)
-  # end
-
-  # # include show routes
-  # get "/aspects/:id" do
-  #   aspect = Aspect.find_by(id: params[:id])
-  #   if aspect
-  #   aspect.to_json(include: :strategies)
-  #   else
-  #     "404 - not found"
-  #   end
-  # end
-
-  # get "/strategies/:id" do
-  #   strategy = Strategy.find_by(id: params[:id])
-  #   if strategy
-  #   strategy.to_json(include: :aspect)
-  #   else 
-  #     "404 - not found"
-  #   end
-  # end
-
-  # include other CRUD routes
   post "/aspects" do
     aspect = Aspect.create(
       name: params[:name]
